7b1d90d492c7a022701cbd447409c2edf213bd76
Ambreen Hasan <ambodumbo@gmail.com>
2014-12-20 15:00:37 -0800

    added solution to http_uppercaserer.js

---
+++ learnyounode-nodeschool/http_uppercaserer.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var map = require('through2-map');
+var port = process.argv[2];
+
+var server = http.createServer(function(req, res){
+  if (req.method !== 'POST') {
+    return res.end("Send me a POST\n")
+  }
+
+  req.pipe(map(function(chunk){
+    return chunk.toString().toUpperCase();
+  })).pipe(res)
+})
+
+server.listen(Number(port))
+
+// req = client to server
+// res = server to client
